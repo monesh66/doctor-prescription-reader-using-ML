4b0c4c064db0e0eebcfc4b3742adb1c0a75be5e6
monesh <78009484+monesh66@users.noreply.github.com>
2023-06-22 15:30:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+import mysql.connector
+from flask_session import Session
+
+
+
+app = Flask(__name__,static_url_path='/static')
+
+
+#==================================== /home page =================================================
+@app.route('/')
+@app.route('/home')
+def home():
+    
+    return render_template('base.html');
